1bfffdd53c6a1310020afa4a16a16b487103352f
SkidRunner <skidrunner@gmail.com>
2018-03-16 16:04:14 -0500

    Create MainActivity.java

---
+++ apk/src/main/java/com/projectzomboid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.projectzomboid;
+
+import com.jme3.app.AndroidHarness;
+
+public class MainActivity extends AndroidHarness {
+  
+  private static final String APP_CLASS = "com.projectzomboid.pzomb.MainApplication";
+  
+  public MainActivity() {
+    appClass = APP_CLASS;
+  }
+  
+}
